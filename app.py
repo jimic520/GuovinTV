05a8ecd117502cad64fce883ff146ffc04ffc8bf
shaping520 <144744478+shaping520@users.noreply.github.com>
2024-04-13 10:14:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import os
-
+import threading
 from flask import Flask, render_template
 from gevent import pywsgi
 from main import UpdateSource
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 
 @app.route('/')
 def index():
-    return render_template('./index.html')
+    us = UpdateSource()
+    thread = threading.Thread(target=us.main)
+    thread.start()
+    return render_template('index.html')
 
 
+@app.route('/tv')
+def tv():
+    if os.path.exists("result.txt"):
+        return "ok"
+    return "请稍候重试"
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(('0.0.0.0', 8989), app)
