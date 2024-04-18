77b3a3ad0484be538b99eb3df767c8e754261cba
shaping520 <144744478+shaping520@users.noreply.github.com>
2024-04-18 19:14:01 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -17,7 +17,7 @@ favorite_list = [
 ]
 favorite_page_num = 3
 default_page_num = 2
-urls_limit = 10
+urls_limit = 20
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
@@ -25,7 +25,7 @@ ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = ["https://live.v1.mk/api/bestv.php?id=wxtyhd8m/8000000"]
 # crawl_type的默认值为1-只爬取foodieguide网站；2-只爬取crawl_urls中配置的网站；3-全部
-crawl_type = "2"
+crawl_type = "3"
 # 收集其他大佬url中的直播源
 crawl_urls = ["https://mirror.ghproxy.com/raw.githubusercontent.com/tianya7981/jiekou/main/0406",
               "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
