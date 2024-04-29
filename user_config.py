2e7c0ce1ea37b154e1e70cadf34d24e010c4c2b1
jimic520 <167943021+jimic520@users.noreply.github.com>
2024-04-29 17:00:32 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -18,7 +18,6 @@ crawl_type = "3"
 crawl_urls = ["https://mirror.ghproxy.com/raw.githubusercontent.com/tianya7981/jiekou/main/0406",
               "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
               "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6Plus.m3u",
-              "http://home.jundie.top:81/Cat/tv/live.txt",
  "https://mirror.ghproxy.com/raw.githubusercontent.com/shidahuilang/shuyuan/shuyuan/iptv.txt"]
 # ftp上传result.txt文件
 ftp_host = ""
